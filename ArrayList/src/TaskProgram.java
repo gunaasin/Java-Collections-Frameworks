6ac7a1542fb564c9c3b4b346a9298d9845d4ccc8
gunaasin <guna.asin06@gmail.com>
2024-11-22 13:09:08 +0530

    Linked List - Collections

--- ArrayList/src/TaskProgram.java
+++ ArrayList/src/TaskProgram.java
@@ -1,7 +1,9 @@
 public class TaskProgram {
     public static  void  main(String[] arg){
         int a = 10;
+        int b = 12;
         Integer i = Integer.valueOf(a);
+        Integer j = Integer.valueOf(b);
         System.out.print(i);
     }
 }
---
+++ LinkedList/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(10);
+        ll.add(20);
+        ll.add(40);
+        ll.add(70);
+        ll.add(10);
+        ll.add(50);
+        System.out.println(ll);
+    }
+
+}
